c80f6f2eb55ccfffa125b5742f6070045ff80622
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-03-07 00:21:21 +0200

    add infrastructure project config

---
+++ Walmart.Infrastructure/ModuleInfrastructureDependencies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Walmart.Application.Abstracts.Persistence;
+using Walmart.Infrastructure.Persistence.Repositories;
+
+namespace Walmart.Infrastructure
+{
+    public static class ModuleInfrastructureDependencies
+    {
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+
+             => services
+                .AddTransient(typeof(IBaseRepository<>), typeof(BaseRepositoryc<>));
+    }
+}
