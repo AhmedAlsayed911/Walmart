ef0ee86fe21252bc25c355cc9eb6c7fc59b4b797
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-03-07 00:21:54 +0200

    add razor views

---
+++ Walmart/Views/Role/_RoleForm.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Walmart.Views.Role
+{
+    public class _RoleFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
