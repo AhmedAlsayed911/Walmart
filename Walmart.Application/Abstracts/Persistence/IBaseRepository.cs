dd0d8c8b2389fc5d0d9542c896b96c6a2b5f84a5
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-03-07 00:21:25 +0200

    add abstracts and pagination

---
+++ Walmart.Application/Abstracts/Persistence/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+
+namespace Walmart.Application.Abstracts.Persistence
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task DeleteRangeAsync(ICollection<T> entities);
+        Task<T> GetByIdAsync(int id);
+        Task SaveChangesAsync();
+        IDbContextTransaction BeginTransaction();
+        void Commit();
+        void RollBack();
+        IQueryable<T> GetTableNoTracking();
+        IQueryable<T> GetTableAsTracking();
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(ICollection<T> entities);
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(ICollection<T> entities);
+        Task DeleteAsync(T entity);
+        Task<T> FindByAsync(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ Walmart.Application/Pagination/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Walmart.Application.Pagination
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(
+            this IQueryable<T> query,
+            int pageNumber,
+            int pageSize,
+            CancellationToken cancellationToken = default)
+        {
+            pageNumber = pageNumber < 1 ? 1 : pageNumber;
+            pageSize = pageSize <= 0 ? 10 : pageSize;
+
+            var totalCount = await query.CountAsync(cancellationToken);
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PaginatedResult<T>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+    }
+}
