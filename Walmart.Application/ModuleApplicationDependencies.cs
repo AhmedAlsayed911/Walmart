b000ab24ab9b07dba9a7f549cf531bd007295ef8
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-03-07 00:21:45 +0200

    add application middlewares and config

---
+++ Walmart.Application/ModuleApplicationDependencies.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Walmart.Application
+{
+    public static class ModuleApplicationDependencies
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+
+            =>
+            services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+            .AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+}
