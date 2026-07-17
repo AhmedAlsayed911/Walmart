c585aab2d4343f67ec5de60d5033e1232c855562
Abo7med <ahmad.sayed.salem11@gmail.com>
2026-07-17 09:23:44 +0300

    Improvments

--- SQLQuery.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT * FROM Security.Users
-SELECT * FROM Security.Roles
-
-INSERT INTO security.UserRoles (UserId , RoleId) SELECT '6db86cf4-5c79-46e0-978b-8be0831682cc', Id FROM security.Roles
-
-DELETE FROM [security].[UserRoles] WHERE UserId = '6db86cf4-5c79-46e0-978b-8be0831682cc'
